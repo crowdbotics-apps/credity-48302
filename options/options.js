f060caf598283e93c21fde02872c294837be915e
Crowdbotics <team@crowdbotics.com>
2024-06-06 17:14:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "credity-48302",
+  url: "https://credity-48302.botics.co",
+  api: "https://credity-48302.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
